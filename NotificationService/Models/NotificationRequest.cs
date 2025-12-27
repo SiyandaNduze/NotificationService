988da90b8fd9ff7696a4dcccd093dbb292b9bf42
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 16:54:43 +0200

    created models with enum notification type

---
+++ NotificationService/Models/NotificationRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace NotificationService.Models
+{
+    public enum NotificationType
+    {
+        Email,
+        Sms,
+        Push
+    }
+
+    public class NotificationRequest
+    {
+        public NotificationType Type { get; set; }
+        public string To { get; set; } = string.Empty;
+        public string? Subject { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
