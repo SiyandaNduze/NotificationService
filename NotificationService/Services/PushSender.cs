721d032f84fa1aeb5a6ef3548e9b829dcbcdfa1a
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 17:47:43 +0200

    Made a push sender class in services folder

---
+++ NotificationService/Services/PushSender.cs
@@ -0,0 +1,36 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using NotificationService.Services.Interfaces;
+
+namespace NotificationService.Services
+{
+    public class PushSender : INotificationSender
+    {
+        public PushSender()
+        {
+            if(FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.GetApplicationDefault()
+                });
+            }
+        }
+
+        public async Task SendAsync(string to, string message, string? subject = null)
+        {
+            var msg = new Message
+            {
+                Token = to,
+                Notification = new Notification
+                {
+                    Title = subject ?? "Notification",
+                    Body = message
+                }
+            };
+
+            await FirebaseMessaging.DefaultInstance.SendAsync(msg);
+        }
+    }
+}
