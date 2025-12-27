46cc494d0633430a425684fae73aa430a8e27373
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 19:28:27 +0200

    added the services in program.cs, provide example layout to connect smtp and twilio

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,12 +1,27 @@
+using NotificationService.Background;
+using NotificationService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<EmailSender>();
+builder.Services.AddSingleton<SmsSender>();
+builder.Services.AddSingleton<PushSender>();
+builder.Services.AddSingleton<NotificationDispatcher>();
+
+builder.Services.AddHostedService<NotificationBackgroundWorker>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
