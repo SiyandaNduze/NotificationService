7787d1bbf458aaddabd1842aab4059b4ad3c3891
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 18:16:59 +0200

    Added a notification dispatcher

---
+++ NotificationService/Services/NotificationDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using NotificationService.Models;
+using NotificationService.Services.Interfaces;
+
+namespace NotificationService.Services
+{
+    public class NotificationDispatcher
+    {
+        private readonly IServiceProvider _provider;
+
+        public NotificationDispatcher(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public async Task DispatchAsync(NotificationRequest request)
+        {
+            INotificationSender sender = request.Type switch
+            {
+                NotificationType.Email => _provider.GetRequiredService<EmailSender>(),
+                NotificationType.Sms => _provider.GetRequiredService<SmsSender>(),
+                NotificationType.Push => _provider.GetRequiredService<PushSender>(),
+                _ => throw new NotSupportedException()
+            };
+
+            await sender.SendAsync(request.To, request.Message, request.Subject);
+        }
+    }
+}
