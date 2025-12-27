8db2f832e53f7e32028e34ab83e44398e06fde86
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 18:30:55 +0200

    Added a notification controller

---
+++ NotificationService/Controllers/NotificationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotificationService.Background;
+using NotificationService.Models;
+
+namespace NotificationService.Controllers
+{
+    [ApiController]
+    [Route("api/notifications")]
+    public class NotificationController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Send(NotificationRequest request)
+        {
+            NotificationBackgroundWorker.Enqueue(request);
+            return Accepted(new { status = "Accepted" });
+        }
+    }
+}
