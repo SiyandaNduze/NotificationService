9c9a80c0a55c7f8475a8ee95c037d0c061f7d798
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 18:25:41 +0200

    Made a background worker for notification

---
+++ NotificationService/Background/NotificationBackgroundWorker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using NotificationService.Models;
+using NotificationService.Services;
+
+namespace NotificationService.Background
+{
+    public class NotificationBackgroundWorker : BackgroundService
+    {
+        private static readonly ConcurrentQueue<NotificationRequest> Queue = new();
+        private readonly NotificationDispatcher _dispatcher;
+
+        public static void Enqueue(NotificationRequest request)
+        {
+            Queue.Enqueue(request);
+        }
+
+        public NotificationBackgroundWorker(NotificationDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (Queue.TryDequeue(out var notification))
+                {
+                    try
+                    {
+                        await _dispatcher.DispatchAsync(notification);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
