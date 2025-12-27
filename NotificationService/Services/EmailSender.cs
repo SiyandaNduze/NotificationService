6eb9f3a93e0b08e16041f91d57a30b9323081cd2
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 17:12:39 +0200

    Made and email sender class in services

---
+++ NotificationService/Services/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+using NotificationService.Services.Interfaces;
+
+namespace NotificationService.Services
+{
+    public class EmailSender : INotificationSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendAsync(string to, string message, string? subject = null)
+        {
+            var smtp = new SmtpClient(
+                _config["Smtp:Host"],
+                int.Parse(_config["Smtp:Port"]!))
+            {
+                Credentials = new NetworkCredential(
+                    _config["Smtp:Username"],
+                    _config["Smtp:Password"]),
+                EnableSsl = true
+            };
+
+            var mail = new MailMessage(
+                _config["Smtp:From"]!,
+                to,
+                subject ?? "Notification",
+                message);
+            
+            await smtp.SendMailAsync(mail);
+        }
+    }
+}
