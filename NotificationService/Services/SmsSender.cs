f8310cc24efd685326628d07386a3209be121c7a
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-27 17:39:25 +0200

    Added a Sms sender class to the services folder

---
+++ NotificationService/Services/SmsSender.cs
@@ -0,0 +1,29 @@
+﻿using NotificationService.Services.Interfaces;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace NotificationService.Services
+{
+    public class SmsSender : INotificationSender
+    {
+        private readonly IConfiguration _config;
+
+        public SmsSender(IConfiguration config)
+        {
+            _config = config;
+            TwilioClient.Init(
+                _config["Twilio:AccountSid"],
+                _config["Twilio:AuthToken"]
+                );
+        }
+
+        public async Task SendAsync(string to, string message, string? subject = null)
+        {
+            await MessageResource.CreateAsync(
+                body: message,
+                from: _config["Twilio:From"],
+                to: to
+                );
+        }
+    }
+}
